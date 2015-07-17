5b474d017fffae67bdca545154328cb4a5dc26ee
Andreas Sorge <as@asorge.de>
2015-07-17 12:31:08 +0200

    Add first draft of structure
    (a Freeplane mindmap)

---
+++ reproducible-hpc-guide.mm
@@ -0,0 +1,94 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1437128524442"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="2">A Pythonic Guide to </font>
+    </p>
+    <p style="text-align: center">
+      <font size="2">Reproducible High-Performance Computing &amp; Simulation Output Analysis</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle" background="#ff3366">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" BACKGROUND_COLOR="#ffffff" STYLE="as_parent">
+<font NAME="Lato" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="Chapters" POSITION="right" ID="ID_1911920291" CREATED="1428993203268" MODIFIED="1428993204731">
+<edge COLOR="#0000ff"/>
+<node TEXT="High-Performance Computing" ID="ID_1321623776" CREATED="1437126844374" MODIFIED="1437126849466">
+<node TEXT="Parallel Random Number Generation" ID="ID_371477232" CREATED="1437125744081" MODIFIED="1437125750118"/>
+<node TEXT="Map/Reduce Variance" ID="ID_978701153" CREATED="1437126889946" MODIFIED="1437126897386"/>
+<node TEXT="gridmap" ID="ID_305593722" CREATED="1437126928311" MODIFIED="1437126931777"/>
+<node TEXT="Grid Engine Environment" ID="ID_95074179" CREATED="1437128391899" MODIFIED="1437128399010"/>
+</node>
+<node TEXT="Simulation Output Analysis" ID="ID_1310038645" CREATED="1437126900885" MODIFIED="1437126906694">
+<node TEXT="Confidence Intervals" ID="ID_1112361667" CREATED="1437126907494" MODIFIED="1437126912198"/>
+<node TEXT="Bootstrapping" ID="ID_1563963055" CREATED="1437126913427" MODIFIED="1437126915394"/>
+</node>
+<node TEXT="Python" ID="ID_1293079337" CREATED="1437126872142" MODIFIED="1437126873972"/>
+<node TEXT="Packaging" ID="ID_1703965793" CREATED="1437126875186" MODIFIED="1437128451959"/>
+<node TEXT="Reproducible Workflow" ID="ID_881759051" CREATED="1437126859275" MODIFIED="1437126866493"/>
+<node TEXT="Jupyter" ID="ID_1798457225" CREATED="1437128358789" MODIFIED="1437128371268">
+<node TEXT="Interactive Grid Engine Usage" ID="ID_613342163" CREATED="1437128376539" MODIFIED="1437128420644"/>
+</node>
+</node>
+<node TEXT="Glossary" POSITION="right" ID="ID_81734343" CREATED="1437128425505" MODIFIED="1437128427720">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
